7d87aba5ffc84f35b596aa066fbae17ec569ddae
Olivier PARPAILLON <olivier.parpaillon2023@campus-eni.fr>
2024-10-04 15:45:15 +0200

    do the pagination

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,12 +8,15 @@ use App\Repository\WishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class MainController extends AbstractController
 {
   private WishRepository $wishRepository;
-  public function __construct(WishRepository $wishRepository) {
+  private HttpClientInterface $httpClient;
+  public function __construct(WishRepository $wishRepository, HttpClientInterface $httpClient) {
     $this->wishRepository = $wishRepository;
+    $this->httpClient = $httpClient;
   }
 
     #[Route('/', name: "main_home", methods: ['GET'])]
@@ -27,5 +30,27 @@ class MainController extends AbstractController
     public function aboutUsPage(): Response
     {
         return $this->render("main/about-us.html.twig");
+    }
+
+    #[Route('/events', name: "events", methods: ['GET'])]
+    public function eventsPage(): Response
+    {
+      $response = $this->httpClient->request('GET', 'https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/evenements-publics-openagenda/records', [
+        'query' => [
+          'limit' => 18,
+          'offset' => 1
+        ]
+      ]);
+      // Vérifie le statut de la réponse et récupère les données
+      if ($response->getStatusCode() === 200) {
+        $totalCount = $response->toArray()['total_count'];
+        $totalPages = $totalCount / 18;
+        $content = $response->toArray(); // Décode directement en tableau associatif
+        return $this->render("main/events.html.twig", [
+          'events' => $content['results'] ?? [],
+          'totalPages' => $totalPages,
+        ]);
+      }
+//      dd($content);
     }
 }
